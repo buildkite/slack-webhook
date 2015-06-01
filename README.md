248ced77c07751c817e33974b90886655f7795a8
Tim Lucas <t@toolmantim.com>
2015-06-02 00:03:40 +1000

    v1 :sparkles:

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 An example [Slack Slash Command](https://api.slack.com/slash-commands) for [Buildkite](https://buildkite.com/).
 
+![](http://i.imgur.com/B8Crsbc.gif)
+
 ## Usage
 
-1. **Deploy it to Heroku** <br>[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+1. **Create a new Slash Command** on Slack. Copy the token.
 
-2. ?
+1. **Deploy it to Heroku** <br>[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
 
 3. **Profit**
 
--- index.js
+++ index.js
@@ -1,32 +1,79 @@
 // Expected env settings
 var slack_token = process.env.SLACK_TOKEN;
 var buildkite_api_token = process.env.BUILDKITE_API_TOKEN;
-var buildkite_org_slug  = process.env.BUILDKITE_ORG_SLUG;
+var buildkite_default_org_slug  = process.env.BUILDKITE_DEFAULT_ORG_SLUG;
 
 var https      = require('https');
 var express    = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.post('/', function(req, res){
 
   console.log('Received POST', req.headers, req.body);
 
-  if (req.params.token != slack_token) {
+  if (req.body.token != slack_token) {
     console.log("Invalid Slack token");
     return res.status(401).send('Invalid token');
   }
 
-  res.send('AOK');
+  var command = req.body.text.split(" ")[0];
+
+  if (command == "build") {
+    build_command(req, res);
+  } else {
+    usage_help(res);
+  }
 });
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('Express listening on port', this.address().port);
 });
 
-function post_to_buildkite(path, params) {
+function usage_help(res) {
+  res.send([
+    "build <org>/<project> \"<message>\" <branch default:master> <commit default:HEAD> (e.g. build spacex/rockets)"
+    // More command help here
+  ].join("\n"));
+}
+
+function build_command(req, res) {
+  var buildCommandMatch = req.body.text.match(/^build (.*?) ["“](.*?)["”](?: ([^ ]+))?(?: ([^ ]+))?/);
+  if (!buildCommandMatch) return usage_help(res);
+
+  var orgProjMatch = buildCommandMatch[1].match(/(.*)\/(.*)/);
+  if (orgProjMatch) {
+    var org = orgProjMatch[1];
+    var project = orgProjMatch[2];
+  } else {
+    var org = buildkite_default_org_slug;
+    var project = buildCommandMatch[1];
+  }
+
+  var message = buildCommandMatch[2];
+  var branch = buildCommandMatch[3] || "master";
+  var commit = buildCommandMatch[4] || "HEAD";
+
+  console.log("Build command", buildCommandMatch, org, project, message);
+
+  post_to_buildkite('/v1/organizations/' + org + '/projects/' + project + '/builds', {
+    branch: branch,
+    commit: commit,
+    message: message
+  }, function(responseCode, responseBody) {
+    console.log("Build API response", responseCode, responseBody)
+    if (responseCode >= 300) {
+      return res.send("Buildkite API failed: " + responseCode + " " + responseBody);
+    } else {
+      var responseJson = JSON.parse(responseBody);
+      return res.send("Build created! " + responseJson.web_url);
+    }
+  });
+}
+
+function post_to_buildkite(path, params, callback) {
   var body = JSON.stringify(params);
 
   console.log("Posting to Buildkite", path, body);
@@ -43,8 +90,13 @@ function post_to_buildkite(path, params) {
       "Authorization":  "Bearer " + buildkite_api_token,
     }
   }, function(res) {
+    var body = "";
     res.on("data", function(data) {
-      console.log("Buildkite API response", res.statusCode, data.toString());
+      body += data.toString();
+    });
+    res.on("end", function() {
+      console.log("Buildkite API response", res.statusCode, body);
+      callback(res.statusCode, body);
     });
   });
 
